993932175ba6835a588705b198c12d423b809420
NivekNK <kevin.rojas.v@mail.pucv.cl>
2023-05-14 15:13:26 -0400

    Fold update

--- lua/plugins/ufo/init.lua
+++ lua/plugins/ufo/init.lua
@@ -15,7 +15,34 @@ local ufo_config = {
     close_fold_kinds = {},
 
     -- A function customize fold virt text, see ### Customize fold text
-    fold_virt_text_handler = nil,
+    -- example from: https://github.com/kevinhwang91/nvim-ufo
+    fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+        local newVirtText = {}
+        local suffix = ('  %d '):format(endLnum - lnum)
+        local sufWidth = vim.fn.strdisplaywidth(suffix)
+        local targetWidth = width - sufWidth
+        local curWidth = 0
+        for _, chunk in ipairs(virtText) do
+            local chunkText = chunk[1]
+            local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            if targetWidth > curWidth + chunkWidth then
+                table.insert(newVirtText, chunk)
+            else
+                chunkText = truncate(chunkText, targetWidth - curWidth)
+                local hlGroup = chunk[2]
+                table.insert(newVirtText, {chunkText, hlGroup})
+                chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                -- str width returned from truncate() may less than 2nd argument, need padding
+                if curWidth + chunkWidth < targetWidth then
+                    suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+                end
+                break
+            end
+            curWidth = curWidth + chunkWidth
+        end
+        table.insert(newVirtText, {suffix, 'MoreMsg'})
+        return newVirtText
+    end,
 
     -- Enable a function with `lnum` as a parameter to capture the virtual text
     -- for the folded lines and export the function to `get_fold_virt_text` field of
